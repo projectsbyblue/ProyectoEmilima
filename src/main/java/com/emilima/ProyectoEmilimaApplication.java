f38be5ed4187e5b86593db64ef240437f243918f
ProjectsByBlue <leonardolopezpeve@hotmail.com>
2022-05-23 14:12:38 -0500

    Inicio del repositorio del proyecto con configuraciones basicas

---
+++ src/main/java/com/emilima/ProyectoEmilimaApplication.java
@@ -0,0 +1,14 @@
+package com.emilima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoEmilimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoEmilimaApplication.class, args);
+		System.out.println("[Main] -- Running");
+	}
+
+}
